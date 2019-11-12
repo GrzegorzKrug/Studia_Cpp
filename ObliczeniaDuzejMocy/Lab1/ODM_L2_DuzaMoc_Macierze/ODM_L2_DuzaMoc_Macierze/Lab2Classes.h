f0dc297e7b09124d25ab07d73c45d3c4b6f9ae90
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-11-12 23:47:54 +0100

    Class

---
+++ ObliczeniaDuzejMocy/Lab1/ODM_L2_DuzaMoc_Macierze/ODM_L2_DuzaMoc_Macierze/Lab2Classes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class CSVReader
+{
+	std::string fileName;
+	std::string delimeter;
+
+public:
+	CSVReader(std::string filename, std::string delm = ",") :
+		fileName(filename), delimeter(delm)
+	{ }
+
+	// Function to fetch data from a CSV File
+	std::vector<std::vector<std::string> > getData();
+};
