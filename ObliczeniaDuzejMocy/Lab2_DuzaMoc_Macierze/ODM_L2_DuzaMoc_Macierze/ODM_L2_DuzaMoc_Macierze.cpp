64ea332bd1b1e04c23bec50e4252221521065378
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-12-06 17:48:26 +0100

    Move files

--- ObliczeniaDuzejMocy/Lab2_DuzaMoc_Macierze/ODM_L2_DuzaMoc_Macierze/ODM_L2_DuzaMoc_Macierze.cpp
+++ ObliczeniaDuzejMocy/Lab2_DuzaMoc_Macierze/ODM_L2_DuzaMoc_Macierze/ODM_L2_DuzaMoc_Macierze.cpp
@@ -19,8 +19,8 @@ int main()
 	//std::vector<std::vector<float>> matrixA = reader.getData("M100_a");
 	//std::vector<std::vector<float>> matrixB = reader.getData("M100_b");
 
-	Matrix* A = new Matrix("M100_a");
-	Matrix* B = new Matrix("M100_b");
+	Matrix* A = new Matrix("matrices\\M100_a");
+	Matrix* B = new Matrix("matrices\\M100_b");
 	Matrix* C = new Matrix(A->get_n(), A->get_m());
 
 	if (A->get_m() == B->get_n())
@@ -41,7 +41,7 @@ int main()
 					//result += matrixA[i][k] * matrixB[k][j];
 					int val = A->get_val(row_A, element) * B->get_val(element, col_B);
 					int pre_val = C->get_val(row_A, col_B);
-					C->set_val(row_A, col_B, pre_val + val);
+					//C->set_val(row_A, col_B, pre_val + val);
 				}
 				//outputRow.push_back(result);
 			}
@@ -57,7 +57,7 @@ int main()
 		//		}
 		//		std::cout << std::endl;
 		//	}
-
+		A->writeToFile();
 		cout << "Time elapsed: " << end_time - begin_time << " ms" << endl;
 	}
 	else
