158434309d4ad475f89feb20829d42acafc2ef0d
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2020-01-08 21:42:54 +0100

    OneFile

--- ObliczeniaDuzejMocy/ODM_Zad_2_Main/ODM_Zad_2_Main/ODM_Zad_2_Main.cpp
+++ ObliczeniaDuzejMocy/ODM_Zad_2_Main/ODM_Zad_2_Main/ODM_Zad_2_Main.cpp
@@ -1,11 +1,9 @@
-// ODM_Zad_2_Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <tbb/tbb.h>
 #include <string.h>
-//#include "zad1.h"
-#include "zad1.cpp"
+
+//#include "zad1.cpp"
 
 using namespace std;
 using namespace tbb;
@@ -23,22 +21,95 @@ float run_10_times(float (*ptr)())
 	return sum_times / N;
 }
 
-
-
-float zad2_parallel_foreach()
+class PrintHello
 {
-	//void (*fun_ptr)() = &function_hello;
+public:
+	void operator() () const
+	{
+		for (int i = 0; i < 8; i++) {
+			string text = "Hello TBB " + to_string(i + 8) + "! ";
+			std::cout << text;
+		};
+	}
 
+};
+static float zad1_lambda()
+{
 	tick_count time0 = tick_count::now();
-	/*tbb::parallel_invoke(
-		,
-		
-	);*/
+	tbb::parallel_invoke(
+		[]() {
+			for (int i = 0; i < 8; i++) {
+				string text = "Hello TBB " + to_string(i) + "! ";
+				std::cout << text;
+			};
+		},
+		[]() {
+			for (int i = 0; i < 8; i++) {
+				string text = "Hello TBB " + to_string(i + 8) + "! ";
+				std::cout << text;
+			};
+		}
+		);
 	tick_count time_end = tick_count::now();
 
 	float duration = (time_end - time0).seconds();
 	return duration;
 }
+
+
+static float zad1_klasa()
+{
+	PrintHello app1;
+	tick_count time0 = tick_count::now();
+	tbb::parallel_invoke(
+		app1,
+		app1
+	);
+	tick_count time_end = tick_count::now();
+
+	float duration = (time_end - time0).seconds();
+	return duration;
+}
+
+
+void function_hello()
+{
+	for (int i = 0; i < 8; i++) {
+		string text = "Hello TBB " + to_string(i + 8) + "! ";
+		std::cout << text;
+	};
+}
+
+
+float zad1_pointer_to_function()
+{
+	void (*fun_ptr)() = &function_hello;
+
+	tick_count time0 = tick_count::now();
+	tbb::parallel_invoke(
+		fun_ptr,
+		fun_ptr
+	);
+	tick_count time_end = tick_count::now();
+
+	float duration = (time_end - time0).seconds();
+	return duration;
+}
+
+
+//float zad2_parallel_foreach()
+//{
+//	//void (*fun_ptr)() = &function_hello;
+//	tick_count time0 = tick_count::now();
+//	/*tbb::parallel_invoke(
+//		,
+//		
+//	);*/
+//	tick_count time_end = tick_count::now();
+//
+//	float duration = (time_end - time0).seconds();
+//	return duration;
+//}
 
 int main()
 {
@@ -46,13 +117,12 @@ int main()
 	pr1();
 	//float(*ptr_zad1_a)() = &zad1_lambda;
 	//float time1_a = run_10_times(ptr_zad1_a);
-	//float time1_a = zad1_lambda();
 
-	//float(*ptr_zad1_b)() = &zad1_klasa;	
-	//float time1_a = run_10_times(ptr_zad1_b); // To nie dzia³a
-	//float time1_b = zad1_klasa();
+	float time1_a = zad1_lambda();
 
-	//float time1_c = zad1_pointer_to_function();
+	float time1_b = zad1_klasa();
+
+	float time1_c = zad1_pointer_to_function();
 
 
 
