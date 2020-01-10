b175f0cce78822febd6074358235e4ce6ca754f9
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2020-01-11 00:27:39 +0100

    Create NewMathClass.h

---
+++ ObliczeniaDuzejMocy/ODM_Zad_2_Main/Zad2_cpp/NewMathClass.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "ABCD.h"
+
+//class SuperMath : public A
+//{
+//public:
+//	SuperMath() {
+//		std::cout << "Creating Super Math";
+//	}
+//
+//	virtual ~SuperMath() {
+//		std::cout << "Removing Super Math";
+//	}
+//
+//	SuperMath& operator=(const SuperMath& other) {
+//
+//	}
+//
+//	SuperMath& operator+=(const SuperMath& other) {
+//
+//	}
+//};
