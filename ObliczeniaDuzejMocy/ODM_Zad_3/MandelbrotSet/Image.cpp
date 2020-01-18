d4982498629649d1a49bc653f8f5629bc4149960
Grzegorz Krug <grzesiekk321@gmail.com>
2020-01-18 13:05:09 +0100

    Update Image.cpp

--- ObliczeniaDuzejMocy/ODM_Zad_3/MandelbrotSet/Image.cpp
+++ ObliczeniaDuzejMocy/ODM_Zad_3/MandelbrotSet/Image.cpp
@@ -1,74 +1,76 @@
 #include "Image.h"
 
 Image::Image(std::string fileName, int width, int height, int maxColor)
-    : width(width), height(height), maxval(maxColor)
+	: width(width), height(height), maxval(maxColor)
 {
-    // open the file
-    file = openFile(fileName);
+	// open the file
+	file = openFile(fileName);
 
-    // create file header string
-    this->fileHeader = "P3 "       
-        + std::to_string(width)    
-        + " "                      
-        + std::to_string(height)   
-        + " "                      
-        + std::to_string(maxval)   
-        + '\n';                    
+	// create file header string
+	this->fileHeader = "P3 "
+		+ std::to_string(width)
+		+ " "
+		+ std::to_string(height)
+		+ " "
+		+ std::to_string(maxval)
+		+ '\n';
 
-    // save the header to the file
-    file << fileHeader;
+	std::string text = "\t" + fileName + "\n";
+	std::cout << text;
+	// save the header to the file
+	file << fileHeader;
 
-    // allocate memory
-    content = new Pixel[width * height];
+	// allocate memory
+	content = new Pixel[width * height];
 }
 
 void Image::saveFile()
 {
-    for (int i = 0; i < width * height; i++)
-    {
-        file << content[i].toString() << (i % width == 0 ? '\n' : ' ');
-    }
+	for (int i = 0; i < width * height; i++)
+	{
+		file << content[i].toString() << (i % width == 0 ? '\n' : ' ');
+	}
 }
 
 void Image::setPixel(int x, int y, int r, int g, int b)
 {
-    content[x + y * width] = Pixel(r, g, b);
+	content[x + y * width] = Pixel(r, g, b);
 }
 
-Image::~Image() 
-{ 
-    file.close(); 
-    delete[] content;
+Image::~Image()
+{
+	file.close();
+	delete[] content;
 }
 
 std::ofstream Image::openFile(std::string fileName)
 {
-    std::ofstream file;
+	std::ofstream file;
 
-    // check file extension in lower case
-    std::string extension;
-    if (fileName.size() > 4)
-    {
-        extension = fileName.substr(fileName.size() - 5, 4);
-        std::transform(extension.begin(), extension.end(), extension.begin(),
-            [](char c) { return std::tolower(c); }
-        );
-    }
-    else
-        extension = fileName;
+	// check file extension in lower case
+	std::string extension;
+	if (fileName.size() > 4)
+	{
+		extension = fileName.substr(fileName.size() - 5, 4);
+		std::transform(extension.begin(), extension.end(), extension.begin(),
+			[](char c) { return std::tolower(c); }
+		);
+	}
+	else
+		extension = fileName;
 
-    // if not included in file name fix it
-    if (extension != ".ppm")
-        fileName += ".ppm";
+	// if not included in file name fix it
+	if (extension != ".ppm")
+		fileName += ".ppm";
 
-    // open the file
-    file.open(fileName, std::ofstream::out);
+	// open the file
+	file.open(fileName, std::ofstream::out);
 
-    if (!file)
-    {
-        std::cout << "Cannot open file: " << fileName << std::endl;
-        exit(0);
-    }
+	if (!file)
+	{
+		std::cout << "Cannot open file: " << fileName << std::endl;
+		exit(0);
+	}
 
-    return file;
+	return file;
 }
